b3d0fa89749b5b4b60c2cc5167c3a795b549e002
Manuel Reich <errnesto@me.com>
2013-01-24 20:41:47 +0100

    re added missing methods to U3

--- GRDM_U3.java
+++ GRDM_U3.java
@@ -37,8 +37,8 @@ public class GRDM_U3 implements PlugIn {
 		"Rot-Kanal", 
 		"Negativ", 
 		"Graustufen",
-		"Binärbild",
-		"Binärbild mit Fehlerdiffusion",
+		"Bin√§rbild",
+		"Bin√§rbild mit Fehlerdiffusion",
 		"Sepia",
 		"Auf 6 Farben reduzieren"
 	};
@@ -48,7 +48,7 @@ public class GRDM_U3 implements PlugIn {
 
 		//IJ.open("/users/barthel/applications/ImageJ/_images/orchid.jpg");
 		//IJ.open("Z:/Pictures/Beispielbilder/orchid.jpg");
-		IJ.open("/home/arnold/Bilder/Bear.jpg");
+		IJ.open("/Applications/ImageJ/Bilder/Bear.jpg");
 
 		
 		GRDM_U3 pw = new GRDM_U3();
@@ -125,7 +125,7 @@ public class GRDM_U3 implements PlugIn {
 
 		private void changePixelValues(ImageProcessor ip) {
 
-			// Array zum Zurückschreiben der Pixelwerte
+			// Array zum Zur√ºckschreiben der Pixelwerte
 			int[] pixels = (int[])ip.getPixels();
 
 			if (method.equals("Original")) {
@@ -187,14 +187,14 @@ public class GRDM_U3 implements PlugIn {
 						int g = (argb >> 8) & 0xff;
 						int b = argb & 0xff;
 						
-						// der grauwert ist der durschnitt aller kanäle
+						// der grauwert ist der durschnitt aller kan√§le
 						int grey = (r + g + b) / 3;
 						
 						pixels[pos] = (0xFF<<24) | (grey << 16) | (grey << 8) | grey;
 					}
 				}
 			} // end "Graustufen"
-			else if (method.equals("Binärbild")) {
+			else if (method.equals("Bin√§rbild")) {
 				for (int y = 0; y < height; y++) {
 					for (int x = 0; x < width; x++) {
 						int pos = y * width + x;
@@ -207,8 +207,8 @@ public class GRDM_U3 implements PlugIn {
 						// durchschnitt (grauwert) berechnen
 						int avg = (r + g + b) / 3;
 						
-						// je nachdem ob näher an schwarz oder weiß
-						// wird der pixel in einer der beiden farben gefärbt
+						// je nachdem ob n√§her an schwarz oder wei√ü
+						// wird der pixel in einer der beiden farben gef√§rbt
 						if (avg >= 128) {
 							avg = 255;
 						} else avg = 0;
@@ -216,8 +216,8 @@ public class GRDM_U3 implements PlugIn {
 						pixels[pos] = (0xFF<<24) | (avg << 16) | (avg << 8) | avg;
 					}
 				}
-			} // end binärbild
-			else if (method.equals("Binärbild mit Fehlerdiffusion")) {
+			} // end bin√§rbild
+			else if (method.equals("Bin√§rbild mit Fehlerdiffusion")) {
 				int lineError = 0;
 				
 				for (int y = 0; y < height; y++) {
@@ -229,15 +229,15 @@ public class GRDM_U3 implements PlugIn {
 						int g = (argb >> 8) & 0xff;
 						int b = argb & 0xff;
 						
-						// der grauwert ist der mittelwert aus allen kanälen
+						// der grauwert ist der mittelwert aus allen kan√§len
 						int avg = (r + g + b) / 3;
 						int color;
 						
-						// wenn grau +/- fehler näher an weiß ist als
-						// an schwarz, soll der pixel weiß werden
+						// wenn grau +/- fehler n√§her an wei√ü ist als
+						// an schwarz, soll der pixel wei√ü werden
 						
 						if (avg + lineError >= 128) {
-							// der wert weiß ist zu hoch
+							// der wert wei√ü ist zu hoch
 							// das korrigieren wir mit einem negativen fehler
 							lineError += avg - 255;
 							color = 255;
@@ -252,15 +252,106 @@ public class GRDM_U3 implements PlugIn {
 					// der fehler wird nach durchgang einer zeile resettet
 					lineError = 0;
 				}
-			} // end "Binärbild mit Fehlerkorrektur"
+			} // end "Bin√§rbild mit Fehlerkorrektur"
+			//Sepia
 			else if (method.equals("Sepia")) {
+				for (int y = 0; y < height; y++) {
+					for (int x = 0; x < width; x++) {
+						int pos = y * width + x;
+						int argb = origPixels[pos];
+
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >> 8) & 0xff;
+						int b = argb & 0xff;
+						// Grauwert
+						int avg = (r + g + b) / 3;
+						//Erhšhen der Rot und Grun Wetre = mehr Gelb
+						int rn = (int) (avg * 1.4);
+						int gn = (int) (avg * 1.4);
+						int bn = (int) (avg);
+						
+						//werte max 255
+						rn = rn >255?255:rn;
+						gn = gn >255?255:gn;
+						bn = bn >255?255:bn;
+
+					//zrŸckscreiben
+						pixels[pos] = (0xFF << 24) | (rn << 16) | (gn << 8)
+								| bn;
+					}
+				}
 				
-			}
+			}//end Sephia
 			else if (method.equals("Auf 6 Farben reduzieren")) {
-				
+				for (int y = 0; y < height; y++) {
+
+					for (int x = 0; x < width; x++) {
+						int pos = y * width + x;
+						int argb = origPixels[pos]; 
+						int r = (argb >> 16) & 0xff;
+						int g = (argb >> 8) & 0xff;
+						int b = argb & 0xff;
+
+						// Variablen Initalisieren
+						int rn = 0;
+						int gn = 0;
+						int bn = 0;
+
+		
+						
+						//Farben wurden manuell ausgewŠhlt mit Photoshop
+						//es wird eine art Abstand zu einer Farbe ŸberprŸft 
+						//und dann je nach dem die angegbene Farbe eingetragen
+						
+						
+						//sehr dunkel
+						if ( r <= 59 && g < 70) {
+							rn = 2;
+							gn = 8;
+							bn = 10;
+						}
+						// dunkles Grau
+						else if (r > 59 && r <= 100) {
+							rn = 30;
+							gn = 30;
+							bn = 30;
+						}
+						// blau
+						else if (((r > 38 && r <= 59) && g > 70)
+								|| ((r > 59 && r <= 90) && b > 90)) {
+							rn = 53;
+							gn = 115;
+							bn = 208;
+						}
+						
+						// braun
+						else if (r > 100 && r <= 120) {
+							rn = 134;
+							gn = 83;
+							bn = 5;
+						}
+						// grau
+						else if (r > 120 && r <= 150) {
+							rn = 160;
+							gn = 160;
+							bn = 160;
+						}
+						// hellgrau
+						else if (r > 150 && r <= 256) {
+							rn = 200;
+							gn = 200;
+							bn = 200;
+						}
+
+						// Hier muessen die neuen RGB-Werte wieder auf den
+						// Bereich von 0 bis 255 begrenzt werden
+
+						pixels[pos] = (0xFF << 24) | (rn << 16) | (gn << 8)
+								| bn;
+					}
+				}//end 6Farben
 			}
 		}
 
-
 	} // CustomWindow inner class
-} 
+}
